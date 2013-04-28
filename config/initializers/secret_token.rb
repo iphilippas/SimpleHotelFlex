4a4bb735cfffe4610c1b974cfd730bc50e63961d
iphilippas@gmail.com <ioannis@Ioannis-Andreass-Mac-mini.local>
2013-04-28 10:47:16 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simplehotelflex::Application.config.secret_token = '042227a3df7c1a2c9a559d499d30933aa1c475d7dfe09fb753818c613974799dcf579062f04d3099282bd58919dc3311c673d5093ad8d475cb2efd7039f03102'
